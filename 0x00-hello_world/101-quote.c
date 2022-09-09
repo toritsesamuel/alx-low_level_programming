a70b929dff5398a52cad199c0053bff1d8cde4c6
toritsesamuel <toritsesamuelots9>
2022-09-09 06:16:47 -0700

    task eight

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
 return (1);
 }
