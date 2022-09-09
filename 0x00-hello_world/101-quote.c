709638ec5ef6456ffdfe05e1efa1d50dc4d17ee2
toritsesamuel <toritsesamuelots9>
2022-09-09 06:34:10 -0700

    task extra

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, quo, 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
