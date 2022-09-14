5f72544a0f784ff190d57768d66d52447cf762ad
toritsesamuel <toritsesamuelots9>
2022-09-14 15:01:18 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-_putchar("");
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -16,6 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
 return (0);
 }
