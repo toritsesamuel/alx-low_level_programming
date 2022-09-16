5a8e8e52aebcb470ec0f5277f1be3506d04e0bb5
toritsesamuel <toritsesamuelots9>
2022-09-16 02:43:16 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+
+while (i < 10)
+
+{
+
+putchar(i);
+
+}
+
+return (0);
+
+}
