001e193b9f889d750cbb41855771141297a04756
toritsesamuel <toritsesamuelots9>
2022-09-19 16:00:26 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every other character of a string,
+ *
+ * Followed by a new line.
+ *
+ * @str: An input string
+ *
+ * Return: Nothing
+ *
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+	_putchar('\n');
+}
