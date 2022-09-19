73b5bdbc0df0bb557ae301eaee7c5cb6ce5816b0
toritsesamuel <toritsesamuelots9>
2022-09-19 15:28:33 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,11 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i = 0;
 
-	while (s[index++])
-		len++;
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	while (s[i] != '\0')
+		i++;
+	while (i)
+		_putchar(s[--i]);
 	_putchar('\n');
 }
