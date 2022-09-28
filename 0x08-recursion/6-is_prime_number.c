489dde767eb363f1e514eec6209551e0ccc9d245
toritsesamuel <toritsesamuelots9>
2022-09-28 15:18:26 -0700

    tasks

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,28 @@
 #include "main.h"
 
 /**
- * sqrt_check - verify if s is the square root of c
+ * prime_check - verify if s is the square root of c
  * @s: parameter
- * @c: number to find sqrt of
- * Return: -1 or sqrt of c
+ * @c: number to find if is a prime numberr
+ * Return: 1 or 0
  */
-
-int sqrt_check(int s, int c)
+int prime_check(int s, int c)
 {
-	if (s * s == c)
-		return (s);
 	if (s * s > c)
-		return (-1);
-	return (sqrt_check(s + 1, c));
+		return (1);
+	if (c % s == 0)
+		return (0);
+	return (prime_check(s + 1, c));
 }
-/**
- * _sqrt_recursion - function that returns the natural root of a number
- * @n: parameter
- * Return: natural root of n
- */
 
-int _sqrt_recursion(int n)
+/**
+ * is_prime_number - function that checks if n is a prime number
+ * @n: parameter
+ * Return: 1 if n is prime number or 0 otherwise
+ */
+int is_prime_number(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (sqrt_check(1, n));
+	if (n <= 1)
+		return (0);
+	return (prime_check(2, n));
 }
