54fcfe25798ad632a45b8ad89ae85dc30ac20973
toritsesamuel <toritsesamuelots9>
2022-10-02 14:57:13 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,17 +17,17 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
-			if (isdigit(argv[i][j]) == 0
-					{
-					puts("Error");
-					return (1);
-					}
-					}
-					}
-					for (i = 1; i < argc; i++)
-					{
-					a += atoi(argv[i]);
-					}
-					printf("%d\n", a);
-					return (0);
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
